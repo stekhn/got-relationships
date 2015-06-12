26671fcda92a6b05faeaba1502331f5c99952540
Steffen Kühne <stekhn@gmail.com>
2015-06-12 14:54:13 +0200

    Add performance mode for mobile

--- js/main.js
+++ js/main.js
@@ -18,8 +18,15 @@ var list = d3.select('.list');
 var slider = d3.select('.slider');
 var sliderWrapper = d3.select('.slider-wrapper');
 var episode = d3.select('.episode');
+var loading = d3.select('.loading');
+
+console.log(getURLParameter('mobile'));
 
 var isDragging = false;
+var isMobile = getURLParameter('mobile') || isMobileBrowser();
+
+
+console.log(isMobile);
 var timeout;
 
 var width = parseInt(container.style('width')),
@@ -47,6 +54,11 @@ slider.on('input', function() {
   resetGraph();
   sortData();
   drawGraph();
+
+  if(!isMobile) {
+    force.start();
+    d3.timer(force.resume);
+  }
 });
 
 close.on('click', function () {
@@ -103,6 +115,8 @@ function sortData() {
     }
   });
 
+  // var sources = [];
+
   relations.forEach(function (relation, i) {
   
     // Any relations with duplicate source and target get an incremented 'linknum'
@@ -129,12 +143,24 @@ function sortData() {
       relation.target = nodes[relation.target];
     }
 
+    // if (relation.source.name) {
+    //   sources.push(relation.source.name);
+    // } else {
+    //   console.log(relation.source);
+    // }
+
     linked[relation.source.name + ',' + relation.target.name] = true;
   });
+
+  // console.log(nodes);
+
+  // for (var node in nodes) {
+  //   if (sources.indexOf(node) < 0) delete nodes[node];    
+  // }
 }
 
 function drawGraph() {
-   force = d3.layout.force()
+  force = d3.layout.force()
       .nodes(d3.values(nodes))
       .links(relations)
       .size([width * 1.3, height])
@@ -161,10 +187,10 @@ function drawGraph() {
       .attr('fill-opacity', '0');
 
   drag = d3.behavior.drag()
-      .origin(function(d) { return d; })
-      .on("dragstart", dragstart)
-      .on("drag", dragging)
-      .on("dragend", dragend);
+    .origin(function(d) { return d; })
+    .on("dragstart", dragstart)
+    .on("drag", dragging)
+    .on("dragend", dragend);
 
   link = svg.append('svg:g').selectAll('path')
       .data(force.links())
@@ -208,6 +234,24 @@ function drawGraph() {
       .attr('x', 14)
       .attr('y', '.4em')
       .text(function(d) { return d.name; });
+
+
+  // Static force layout for mobile devices
+  // http://bl.ocks.org/mbostock/1667139
+  if (isMobile) {
+    loading.style('display', 'block');
+
+    setTimeout(function() {
+      force.start();
+      
+      for (var i = 100; i > 0; --i) {
+        force.tick(true);
+      }
+      
+      loading.style('display', 'none');
+      force.stop();
+    }, 10);
+  }
 }
 
 function scale() {
@@ -216,15 +260,17 @@ function scale() {
       ' scale(' + d3.event.scale + ')');
 }
 
-function tick() {
-  var q = d3.geom.quadtree(nodes),
-      i = 0,
-      n = nodes.length;
+function tick(enforce) {
+  if (enforce || !isMobile) {
+    var q = d3.geom.quadtree(nodes),
+        i = 0,
+        n = nodes.length;
 
-  while (++i < n) q.visit(collide(nodes[i]));
+    while (++i < n) q.visit(collide(nodes[i]));
 
-  link.attr('d', drawLinks);
-  node.attr('transform', drawNode);
+    link.attr('d', drawLinks);
+    node.attr('transform', drawNode);
+  }
 }
 
 function resetGraph() {
@@ -234,11 +280,9 @@ function resetGraph() {
   linked = [];
 
   //@TODO Rather remove single nodes manually
-  nodes = [];
+  nodes = {};
   characters = [];
   relations = [];
-  force.start();
-  d3.timer(force.resume);
 }
 
 // Use elliptical arc path segments to doubly-encode directionality.
@@ -292,8 +336,11 @@ function dragend(d) {
   isDragging = false;
   d3.select(this).classed("dragging", false);
   tick();
-  force.resume();
 
+  if (!isMobile) {
+    force.resume();
+  }
+  
   connectedNodes(d);
 }
 
@@ -449,4 +496,8 @@ function getURLParameter(name) {
     new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)')
       .exec(location.search) || [,""])[1]
       .replace(/\+/g, '%20')) || null;
+}
+
+function isMobileBrowser() {
+  return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)));
 }
