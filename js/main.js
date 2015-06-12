819d3496c569d0e77633eb94f5b6a603762649ed
Steffen Kühne <stekhn@gmail.com>
2015-06-12 09:21:52 +0200

    Add collision detection

--- js/main.js
+++ js/main.js
@@ -203,6 +203,12 @@ function scale() {
 }
 
 function tick() {
+  var q = d3.geom.quadtree(nodes),
+      i = 0,
+      n = nodes.length;
+
+  while (++i < n) q.visit(collide(nodes[i]));
+  
   link.attr('d', drawLinks);
   node.attr('transform', drawNode);
 }
@@ -235,22 +241,24 @@ function drawNode(d) {
 }
 
 function connectedNodes(d) {
-  if (d !== null && !isDragging) {
+  if (!isDragging) {
+    if (d !== null) {
 
-    //Reduce the opacity of all but the neighbouring nodes and the source node
-    node.style('opacity', function (o) {
+      //Reduce the opacity of all but the neighbouring nodes and the source node
+      node.style('opacity', function (o) {
 
-      // Highlight outgoing relations
-      return d.name==o.name | neighboring(d, o)  ? 1 : 0.1;
-    });
-    link.style('opacity', function (o) {
+        // Highlight outgoing relations
+        return d.name==o.name | neighboring(d, o)  ? 1 : 0.1;
+      });
+      link.style('opacity', function (o) {
 
-      // Highlight outgoing relations
-      return d.name==o.source.name ? 1 : 0.05;
-    });
-  } else {   
-    node.style('opacity', 1);
-    link.style('opacity', 0.25);
+        // Highlight outgoing relations
+        return d.name==o.source.name ? 1 : 0.05;
+      });
+    } else {   
+      node.style('opacity', 1);
+      link.style('opacity', 0.25);
+    }
   }
 }
 
@@ -271,6 +279,32 @@ function dragend(d) {
   d3.select(this).classed("dragging", false);
   tick();
   force.resume();
+
+  connectedNodes(d);
+}
+
+function collide(node) {
+  var r = node.radius + 16,
+      nx1 = node.x - r,
+      nx2 = node.x + r,
+      ny1 = node.y - r,
+      ny2 = node.y + r;
+  return function(quad, x1, y1, x2, y2) {
+    if (quad.point && (quad.point !== node)) {
+      var x = node.x - quad.point.x,
+          y = node.y - quad.point.y,
+          l = Math.sqrt(x * x + y * y),
+          r = node.radius + quad.point.radius;
+      if (l < r) {
+        l = (l - r) / l * 0.5;
+        node.x -= x *= l;
+        node.y -= y *= l;
+        quad.point.x += x;
+        quad.point.y += y;
+      }
+    }
+    return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+  };
 }
 
 function displayInfo(d) {
